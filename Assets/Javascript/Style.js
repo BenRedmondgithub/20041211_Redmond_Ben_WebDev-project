302f24b2c833088bd312edad45c8fac39bbc806b
benmakescode <73973708+BenRedmondgithub@users.noreply.github.com>
2024-12-09 20:50:16 +0000

    Adding new style to all main text

--- Assets/Javascript/Style.js
+++ Assets/Javascript/Style.js
@@ -3,3 +3,16 @@ document.getElementById("hamburger").addEventListener("click", function () {
     menu.classList.toggle("show");
 });
 
+var form = document.getElementById("contact-form");
+
+form.addEventListener ("submit", function() {
+    var fullname = document. getElementById("fullname").value;
+    if (fullname != "") {
+        return true
+    }
+    else {
+        alert ("You can't submit the form without a full name entered");
+        event.preventDefault();
+        return false;
+    }
+});
